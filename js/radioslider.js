43025e80414bbff1f7a17f1ed8bc7854d7b19050
JoeyHarms <joey.harms@student.nhlstenden.com>
2024-10-14 12:15:40 +0200

    added slider
    slider at page is added, still problem with colors, will be fixed

---
+++ js/radioslider.js
@@ -0,0 +1,12 @@
+var radiotoggle = false;
+
+if (radiotoggle == false)
+{
+    document.getElementById('slider round').style.display = block;
+    radiotoggle = true;
+}
+else
+{
+    document.getElementById('slider round').style.display = none;
+    radiotoggle = false;
+}
