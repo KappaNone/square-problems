db00140d94beb8a9a86fb5cbcd1ae44ba85f6e3b
JoeyHarms <joey.harms@student.nhlstenden.com>
2024-10-24 12:15:44 +0200

    deleted extra files in /NL/ folder

--- NL/contact.php
+++ NL/contact.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Circle Solutions | Contact</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
-    <link rel="stylesheet" href="styles/global.css" />
-    <link rel="stylesheet" href="styles/contactPage.css" />
+    <link rel="stylesheet" href="../styles/global.css" />
+    <link rel="stylesheet" href="../styles/contactPage.css" />
   </head>
   <body>
     <?php render_dutch_header(); ?>
